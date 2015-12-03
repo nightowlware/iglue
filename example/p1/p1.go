d464dc22121ea73c3d03965e31bb58e708eb116d
Shafik <shafik23@gmail.com>
2015-12-03 00:13:02 -0700

    Added very basic example: p1 sends messages to p2.

--- example/p1/p1.go
+++ example/p1/p1.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/nightowlware/iglue/example"
+	"github.com/nightowlware/iglue"
 )
-
 
 func main() {
 	fmt.Println("Process1 starting ...")
+
+	_, err, statusptr := iglue.Register("p1")
+	defer iglue.Unregister("p1")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 1000 && *statusptr == true; i++ {
+		msg := iglue.Msg{"Header", fmt.Sprintf("Yo, this is p1, message # %d", i)}
+		fmt.Println("P1: Sending to p2: ", msg.String())
+
+		err := iglue.Send(&msg, "p2")
+		if err != nil {
+			panic(fmt.Errorf("Seems like p2 is not running yet; start p2 first then try again.\n%s", err))
+		}
+	}
 }
-
-
--- example/p2/p2.go
+++ example/p2/p2.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/nightowlware/iglue/example"
+	"github.com/nightowlware/iglue"
 )
 
-
 func main() {
-	fmt.Println("Process2 starting ...")
+	fmt.Println("Process2 starting, waiting for Process1 ...")
+
+	channel, err, statusptr := iglue.Register("p2")
+	defer iglue.Unregister("p2")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for *statusptr == true {
+		msg := <-channel
+		fmt.Println("P2: Received from p1: ", msg.String())
+	}
 }
-
-
